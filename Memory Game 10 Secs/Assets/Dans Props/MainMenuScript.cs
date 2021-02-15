52b6b923579675b2394d4d6f9fdebccd982f1510
Dan <DucatiDan@outlook.com>
2021-02-15 15:03:22 +1300

    Start Screen & Guessing Screen

---
+++ Memory Game 10 Secs/Assets/Dans Props/MainMenuScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    public void Game()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Start Screen");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
